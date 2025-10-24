5c2f847eaf523af78c4fca0eca72b6b29d549937
Mahdi <alom4hdi@gmail.com>
2025-10-24 03:58:47 -0700

    del: all

--- lib/rubitex.rb
+++
@@ -1,6 +0,0 @@
-# lib/rubitex.rb
-require_relative "rubitex/client"
-require_relative "rubitex/utils"
-require_relative "rubitex/types"
-require_relative "rubitex/builders"
-require_relative "rubitex/api"
--- lib/rubitex/utils.rb
+++
@@ -1,9 +0,0 @@
-require 'rotp'
-
-module Rubitex
-  class Utils
-    def self.generate_otp(secret)
-      ROTP::TOTP.new(secret, issuer: "Rubitex").now
-    end
-  end
-end
